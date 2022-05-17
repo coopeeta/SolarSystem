563b67e9434afe04e5561a89aafa021e09988d29
coopeeta <coopeeta@gmail.com>
2022-05-17 19:17:28 -0400

    Solar System Project ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.sun = "1";
+        FactorX.moon = "2";
+        FactorX.planet = "5";
+        FactorX.star = "1001";
+
+    }
+}
